b6a16d1c9633fb32c820988d91cec0fc0921a6a2
ciroumek <132610517+ciroumek@users.noreply.github.com>
2023-06-27 15:25:57 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,12 @@
+
+    function cambiocolores()
+        {
+          document.body.style.backgroundColor = 'black';
+          document.body.style.color = 'white';
+            document.getElementById("header").style.backgroundColor = "black";
+            document.getElementById("section").style.backgroundColor = "black";
+           document.getElementById("footer").style.backgroundColor = "black";
+        }
+function botoncopyright() {
+  alert("Derechos de autor pertenecientes a Ciro Umek EMAIL:ciroumeke2@gmail.com")
+}
